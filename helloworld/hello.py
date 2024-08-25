9a94f4db0f3e1a15d74caebcb8f72b540d1b6260
swoldemichael <peace.wolde@gmail.com>
2024-08-25 16:13:52 -0700

    added helloworld deployment

---
+++ helloworld/hello.py
@@ -0,0 +1,19 @@
+import requests
+from starlette.requests import Request
+from typing import Dict
+
+from ray import serve
+
+
+# 1: Define a Ray Serve application.
+@serve.deployment
+class HelloWorld:
+    def __init__(self, msg: str):
+        # Initialize model state: could be very large neural net weights.
+        self._msg = msg
+
+    def __call__(self, request: Request) -> Dict:
+        return {"result": self._msg}
+
+
+app = HelloWorld.bind(msg="Hello world!")
